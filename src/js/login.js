09db6be80b8e078efddb0122a6c81c5e3d1a46a9
Maria Rosa <ross.zhena@gmail.com>
2018-07-31 18:13:04 -0500

    resolviendo conflicto merge

---
+++ src/js/login.js
@@ -0,0 +1,12 @@
+
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyBONnPcYzSFo2OejnVb0-V9yuGW0shURWg",
+        authDomain: "redsocial-3d9dd.firebaseapp.com",
+        databaseURL: "https://redsocial-3d9dd.firebaseio.com",
+        projectId: "redsocial-3d9dd",
+        storageBucket: "redsocial-3d9dd.appspot.com",
+        messagingSenderId: "870956174943"
+      };
+      firebase.initializeApp(config);
+   
